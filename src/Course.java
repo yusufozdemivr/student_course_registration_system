b881d92a960027f55431bea82ecfd03cdc521252
yusufozdemivr <yusufozdemir.ist@gmail.com>
2025-12-05 20:11:37 +0300

    Add basic Course class

---
+++ src/Course.java
@@ -0,0 +1,25 @@
+public class Course {
+
+    private String code;
+    private String name;
+    private int credit;
+
+    public Course(String code, String name, int credit) {
+        this.code = code;
+        this.name = name;
+        this.credit = credit;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+}
