b2991e185d767457454f1ceb955a728b728f22ac
yusufozdemivr <yusufozdemir.ist@gmail.com>
2025-12-09 02:50:25 +0300

    added course methods and tuition to Student class

--- src/Student.java
+++ src/Student.java
@@ -12,5 +12,27 @@ public class Student {
         this.name = name;
     }
 
-}
+    public String getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void registerCourse(Course course) {
+        registeredCourses.add(course);
+    }
+
+    public void dropCourse(Course course) {
+        registeredCourses.remove(course);
+    }
+
+    public List<Course> getRegisteredCourses() {
+        return registeredCourses;
+    }
+
+    public double calculateTuition() {
+        return 1000.0;
+    }
+}
