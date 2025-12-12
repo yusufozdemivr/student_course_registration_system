7d5a70b1f670398750e5a4cdf3b0e084706f321e
yusufozdemivr <yusufozdemir.ist@gmail.com>
2025-12-12 18:02:25 +0300

    overrided credit based tuition calculation in GraduateStudent

--- src/GraduateStudent.java
+++ src/GraduateStudent.java
@@ -3,4 +3,12 @@ public class GraduateStudent extends Student {
     public GraduateStudent(String id, String name) {
         super(id, name);
     }
+
+    @Override
+    public double calculateTuition(){
+        double baseFee = 800.0;
+        double feePerCredit = 150.0;
+
+        return baseFee + getTotalCredits() * feePerCredit;
+    }
 }
