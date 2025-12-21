0c1141630d02ac4407a524be612574d4221fbd17
yusufozdemivr <yusufozdemir.ist@gmail.com>
2025-12-21 14:58:08 +0300

    added demo flow for student course registration in Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+
+        Course c1 = new Course("BLML101", "Algoritma programlama", 4);
+        Course c2 = new Course("BLML102", "Nesne tabanli pgramlama", 5);
+
+        CourseCatalog catalog = new CourseCatalog();
+        catalog.addCourse(c1);
+        catalog.addCourse(c2);
+
+        Student s1 = new Student("S1", "Yusuf");
+        GraduateStudent g1 = new GraduateStudent("S2", "Ahmet");
+
+        s1.registerCourse(c1);
+        s1.registerCourse(c2);
+
+
+        try {
+            s1.registerCourse(c1);
+        } catch (Exception e) {
+            System.out.println("Duplicate registration blocked: " + e.getMessage());
         }
+
+
+        System.out.println("Student courses: " + s1.getRegisteredCourses());
+
+
+        System.out.println("Student tuition: " + s1.calculateTuition());
+        System.out.println("Graduate tuition: " + g1.calculateTuition());
+
+
+        s1.dropCourse(c1);
+        System.out.println("After drop: " + s1.getRegisteredCourses());
     }
 }
